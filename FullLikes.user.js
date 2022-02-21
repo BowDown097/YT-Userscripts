cc98cf1929da853f49760c9dc23357270863e536
BowDown097 <42720004+BowDown097@users.noreply.github.com>
2022-02-20 20:02:26 -0800

    Fix live stream problems and change some selectors

--- FullLikes.user.js
+++ FullLikes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Full Likes
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Restore full like counts.
 // @author       BowDown097
 // @match        *://*.youtube.com/*
@@ -30,15 +30,33 @@ function waitForElement(selector) {
     });
 }
 
+// this observer disables the like count updating while watching a live stream because it messes with a bunch of things and we can't get full like count from it either
+var likeObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        for (const node of mutation.addedNodes) {
+            while (mutation.target.childNodes.length > 1) {
+                mutation.target.removeChild(mutation.target.lastChild);
+            }
+        }
+    });
+});
+
+waitForElement("#info ytd-toggle-button-renderer.style-text[is-icon-button]:first-child #text.ytd-toggle-button-renderer").then(function(elm) {
+    likeObserver.observe(elm, {
+        childList: true,
+        subtree: true
+    });
+});
+
 // thanks objectful
 function getLikes() {
     const topLevelButtons = document.querySelector("ytd-app").data.response.contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.videoActions.menuRenderer.topLevelButtons[0];
     const buttonText = topLevelButtons.isToggled ? topLevelButtons.toggleButtonRenderer.toggledText : topLevelButtons.toggleButtonRenderer.defaultText;
     const likes = parseInt(buttonText.accessibility.accessibilityData.label.replace(/( likes|,)/g, ""));
-    const likesTextElm = document.querySelector("yt-formatted-string#text.ytd-toggle-button-renderer");
+    const likesTextElm = document.querySelector("#info ytd-toggle-button-renderer.style-text[is-icon-button]:first-child #text.ytd-toggle-button-renderer");
     likesTextElm.innerHTML = likes.toLocaleString();
 
-    document.querySelector("ytd-toggle-button-renderer.style-scope:nth-child(1)").addEventListener("click", function() {
+    document.querySelector("ytd-toggle-button-renderer.style-scope:first-child").addEventListener("click", function() {
         const liked = likesTextElm.classList.contains("style-default-active");
         if (liked) {
             likesTextElm.innerHTML = (likes + 1).toLocaleString();
@@ -54,9 +72,10 @@ async function getDislikes(elm) {
     let response = await fetch(`https://returnyoutubedislikeapi.com/votes?videoId=${videoId}`).then((response) => response.json()).catch();
     if (response === undefined || "traceId" in response) return;
 
-    const dislikeText = document.querySelector("ytd-toggle-button-renderer.style-scope:nth-child(2) > a:nth-child(1) > yt-formatted-string:nth-child(2)");
+    const dislikeText = document.querySelector("#info ytd-toggle-button-renderer.style-text[is-icon-button]:nth-child(2) #text.ytd-toggle-button-renderer");
     if (response.dislikes == 0 && response.likes == 0 && response.viewCount == 0) { // no ratings
         dislikeText.innerHTML = "";
+        document.querySelector("#info ytd-toggle-button-renderer.style-text[is-icon-button]:first-child #text.ytd-toggle-button-renderer").innerHTML = "";
     } else { // we have ratings!
         dislikeText.innerHTML = response.dislikes.toLocaleString();
         document.querySelector("ytd-toggle-button-renderer.style-scope:nth-child(2)").addEventListener("click", async function() {
