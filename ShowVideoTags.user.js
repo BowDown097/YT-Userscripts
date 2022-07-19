93a2a91055c77b77ff6039a95db38a7431a5fc45
BowDown097 <42720004+BowDown097@users.noreply.github.com>
2022-07-19 14:45:55 -0700

    Update ShowVideoTags.user.js
    FUCK. YOUTUBE. FUCK THEM SO MUCH. This should account for ALL FOUR!!!! different ways the watch page can be presented. YouTube, just make there only be one. Preferably the one that doesn't have the retarded popout and doesn't look uber ugly.

--- ShowVideoTags.user.js
+++ ShowVideoTags.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      BowDown097
 // @description Shows YouTube video tags in the description.
 // @icon        https://www.google.com/s2/favicons?domain=youtube.com
@@ -12,37 +12,62 @@
 
 function createDescriptionSection(items, tags) {
     const sectionRenderer = document.createElement("ytd-video-description-course-section-renderer"); 
-    sectionRenderer.className = "style-scope ytd-structured-description-content-renderer";
+    sectionRenderer.className = "style-scope ytd-structured-description-content-renderer tags-section";
     items.appendChild(sectionRenderer);
     
     const title = sectionRenderer.querySelector("#title");
-    title.innerHTML = "Tags: " + tags.join(", ");
+    const tagsString = tags.length ? tags.join(", ") : "None";
+    title.innerHTML = `Tags: ${tagsString}`;
     title.style = "font-size: inherit; line-height: inherit; max-height: fit-content";
 }
 
-function showTags() {
+async function showTags() {
     if (window.location.pathname != "/watch")
         return;
-
-    const tags = document.querySelector("ytd-app").data.playerResponse.videoDetails.keywords;
-    if (tags.length == 0)
-        return;
-
-    const descContent = document.querySelector("#items.ytd-structured-description-content-renderer");
-    if (descContent !== null) {
-        createDescriptionSection(descContent, tags);
-    } else {
-        const customDescContent = document.createElement("ytd-structured-description-content-renderer");
-        customDescContent.className = "style-scope ytd-video-secondary-info-renderer";
-        customDescContent.setAttribute("inline-structured-description", "");
-        
-        const customItems = document.createElement("div");
-        customItems.id = "items";
-        customItems.className = "style-scope ytd-structured-description-content-renderer";
-        
-        createDescriptionSection(customItems, tags);
+    
+    [...document.getElementsByClassName("tags-section")].forEach(e => e.remove());
+    var tags = document.querySelector("ytd-app").data.playerResponse.videoDetails.keywords;
+    if (!Array.isArray(tags))
+        tags = new Array();
+    
+    const usingWatchMetadata = document.querySelector("ytd-watch-metadata")?.offsetParent !== null;
+    const descContent = usingWatchMetadata
+        ? document.querySelector("ytd-watch-metadata ytd-structured-description-content-renderer")
+        : document.querySelector("ytd-video-secondary-info-renderer ytd-structured-description-content-renderer");
+    descContent.removeAttribute("hidden");
+    
+    const usingPopoutExperiment = !usingWatchMetadata && descContent.hasAttribute("disable-upgrade");
+    if (usingPopoutExperiment)
+    {
+        await new Promise(resolve => setTimeout(resolve, 500));
+        if (document.querySelector('ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-structured-description"]') === null) // check for manual disabling of experiment
+        {
+            descContent.removeAttribute("disable-upgrade");
+            descContent.setAttribute("inline-structured-description", "");
+            createDescriptionSection(descContent.querySelector("#items"), tags);
+        }
+        else
+        {
+            createDescriptionSection(document.querySelector("ytd-engagement-panel-section-list-renderer #items.ytd-structured-description-content-renderer"), tags);
+        }
     }
-}
+    else
+    {
+        const descItems = descContent.querySelector("#items");
+        if (descItems !== null) {
+            descItems.removeAttribute("hidden");
+            createDescriptionSection(descItems, tags);
+        } else {
+            const customItems = document.createElement("div");
+            customItems.id = "items";
+            customItems.className = "style-scope ytd-structured-description-content-renderer";
+            if (usingWatchMetadata)
+                descContent.appendChild(customItems);
+
+            createDescriptionSection(customItems, tags);
+        }
+    }
+} 
 
 (function() {
     'use strict';
